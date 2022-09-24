fa75b1dd43fd6fe606c2f34609dba45a07d41457
Isaak Haas <isaakhaas96@gmail.com>
2022-09-23 19:50:15 -0500

    feat: change of enum for sealed class

--- app/src/main/java/com/example/jetexpensesapp/navigation/ExpensesScreens.kt
+++ app/src/main/java/com/example/jetexpensesapp/navigation/ExpensesScreens.kt
@@ -1,10 +1,14 @@
 package com.example.jetexpensesapp.navigation
 
 import androidx.annotation.StringRes
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ShowChart
+import androidx.compose.ui.graphics.vector.ImageVector
 import com.example.jetexpensesapp.R
-import java.lang.IllegalArgumentException
 
-sealed class Screen(val route: String, @StringRes val resourceId: Int) {
-    object UdiHomeScreen : Screen("udihome", R.string.udi_home)
-    object AddRetirementEntryScreen : Screen("addretiremententry", R.string.add_retirement_entry)
+sealed class Screen(val route: String, @StringRes val resourceId: Int, val icon: ImageVector? = null) {
+    object UdiHomeScreen : Screen("udihome", R.string.udi_home, Icons.Filled.ShowChart)
+    object AddRetirementEntryScreen :
+        Screen("addretiremententry", R.string.add_retirement_entry)
 }
