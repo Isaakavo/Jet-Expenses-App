5e7a5e5670896b6c6ed80f15502c31c4d5076231
Isaak Haas <isaakhaas96@gmail.com>
2022-11-20 14:07:02 -0600

    feat: add password option

--- app/src/main/java/com/example/jetexpensesapp/components/Login.kt
+++ app/src/main/java/com/example/jetexpensesapp/components/Login.kt
@@ -2,10 +2,12 @@ package com.example.jetexpensesapp.components
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import com.example.jetexpensesapp.components.shared.Button
 
 @Composable
-fun Login (
+fun Login(
     username: String,
     password: String,
     onUsernameChange: (String) -> Unit,
@@ -15,7 +17,13 @@ fun Login (
 
     Column() {
         RetirementInputText(text = username, label = "email", onTextChange = onUsernameChange)
-        RetirementInputText(text = password, label = "password", onTextChange = onPasswordChange)
+        RetirementInputText(
+            text = password,
+            label = "password",
+            onTextChange = onPasswordChange,
+            keyboardType = KeyboardType.Password,
+            visualTransformation = PasswordVisualTransformation()
+        )
         Button(onClick = onLogin, text = "Login")
     }
 }
